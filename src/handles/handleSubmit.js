505b688f6d7757c908ce0272439dafaaeced1383
Tushar H <tusharjain2201@gmail.com>
2024-12-22 09:00:59 +0530

    timestamp

--- src/handles/handleSubmit.js
+++ src/handles/handleSubmit.js
@@ -2,8 +2,9 @@ import { addDoc, collection } from "@firebase/firestore"
 import { firestore } from "../firebase_setup/firebase"
  
 const handleSubmit = (testdata) => {
-    const ref = collection(firestore, "test_data")  // Firebase creates this automatically
- 
+    let timestamp = new Date();
+
+    const ref = collection(firestore, timestamp.toUTCString());  // Firebase creates this automatically
     let data = testdata;
     
     console.log({ data });
